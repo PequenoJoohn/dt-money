f4877bde424915aed5578eae2f1ba2aec2711698
jonathanlm <jonathan.matos@frncubo.com>
2021-12-17 08:48:41 -0300

    feat: pass new date

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -19,10 +19,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        createTransaction({ title, amount, type, category })
+        await createTransaction({ title, amount, type, category });
+
+        
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
